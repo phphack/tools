5758ab9a028f5625e3bbda898db88edd9121ea93
Jianzhi Li <873808813@qq.com>
2020-05-19 16:07:45 +0800

    fix

--- tbl64.php
+++ tbl64.php
@@ -1,15 +1,8 @@
-<style>
-@media (prefers-color-scheme: dark)
-{
-    body {background:#232323;color:rgba(255,255,255,0.8);font:16px Consolas;}
-}
-</style>
-
 <?php
 
 $mod = 64;
-$sql = "ALTER TABLE `gugu_visitor_contribution_new_%s` ADD COLUMN `is_show`  tinyint UNSIGNED NOT NULL DEFAULT 1 COMMENT '是否在排行榜显示 @1:是 @0:否' AFTER `mtime`;";
-
+$sql = "ALTER TABLE `malle_video`.`gugu_short_video_like_%s` ADD INDEX `idx_gmt_create`(`gmt_create`);";
+/*
 $sql = "
 CREATE TABLE `gugu_short_video_like_%s` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
@@ -19,11 +12,10 @@ CREATE TABLE `gugu_short_video_like_%s` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_video` (`user_id`,`video_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='短视频|短视频点赞表';
-";
+";*/
 
 for ($i = 0; $i < $mod; $i++) {
     $suffix = str_pad($i, 2, '0', STR_PAD_LEFT);
     $new = sprintf($sql, $suffix);
     echo $new . PHP_EOL;
 }
-?>
