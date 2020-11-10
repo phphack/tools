65c6173e82399b64561553074bc7b37c8c2a8cbf
lijianzhi <873808813@qq.com>
2020-11-10 15:25:38 +0800

    U

--- tbl_mod.php
+++ tbl_mod.php
@@ -1,7 +1,6 @@
 <?php
 
-$mod = 64;
-$sql = "ALTER TABLE `malle_video`.`gugu_short_video_like_%s` ADD INDEX `idx_gmt_create`(`gmt_create`);";
+$sql = "ALTER TABLE `malle_video`.`gugu_short_video_like_%02d` ADD INDEX `idx_gmt_create`(`gmt_create`);";
 /*
 $sql = "
 CREATE TABLE `gugu_short_video_like_%s` (
@@ -14,8 +13,7 @@ CREATE TABLE `gugu_short_video_like_%s` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='短视频|短视频点赞表';
 ";*/
 
-for ($i = 0; $i < $mod; $i++) {
-    $suffix = str_pad($i, 2, '0', STR_PAD_LEFT);
-    $new = sprintf($sql, $suffix);
-    echo $new . PHP_EOL;
+$mod_num = 64;
+for ($i = 0; $i < $mod_num; $i++) {
+    echo sprintf($sql, $i) . PHP_EOL;
 }
